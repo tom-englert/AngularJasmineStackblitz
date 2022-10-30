bc5a6e1378ce25968b5199b7843fff88e5bc7f96
tom-englert <mail@tom-englert.de>
2022-10-30 14:31:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AngularJasmineStackblitz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-miarvh)
---
+++ src/global.spec.ts
@@ -0,0 +1,8 @@
+describe('This Test', () => {
+  it('succeeds', () => {
+    expect(true).toBe(true);
+  });
+  it('or fails', () => {
+    expect(true).toBe(false);
+  });
+});
